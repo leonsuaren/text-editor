effefcaab9d8560083fa9a680f9a529ffd643adb
Leon Suarez <leonsua@gmail.com>
2023-02-27 18:24:04 -0500

    define container size responsive

--- src/layout/styled.js
+++ src/layout/styled.js
@@ -2,4 +2,24 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin: 0 300px;
+
+  @media (min-width: 320px) and (max-width:480px ) {
+    margin: 0 10px;
+  }
+
+  @media (min-width: 481px) and (max-width: 768px) {
+    margin: 0 50px;
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    margin: 0 100px;
+  }
+
+  @media (min-width: 1025px) and (max-width: 1200px) {
+    margin: 0 200px;
+  }
+
+  @media (min-width: 1201px) {
+    margin: 0 300px;
+  }
 `;
